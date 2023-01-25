ac2bc93ffecf8d8878144c4b33f65660f109fbd5
LeoDominici71 <leonardo_requena@outlook.com>
2023-01-25 18:48:26 +0000

    insert category

--- src/main/java/com/ProjectDsCatalog/DsCatalog/services/CategoryService.java
+++ src/main/java/com/ProjectDsCatalog/DsCatalog/services/CategoryService.java
@@ -1,6 +1,5 @@
 package com.ProjectDsCatalog.DsCatalog.services;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -34,6 +33,13 @@ public class CategoryService {
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
 		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);
+	}
+	@Transactional(readOnly = true)
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
 		return new CategoryDTO(entity);
 	}
 
