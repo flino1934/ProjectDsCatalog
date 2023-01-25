8d6dd6a436672c0ca0e433255a2287c62335364b
LeoDominici71 <leonardo_requena@outlook.com>
2023-01-25 23:12:56 +0000

    deletando com delete

--- src/main/java/com/ProjectDsCatalog/DsCatalog/services/CategoryService.java
+++ src/main/java/com/ProjectDsCatalog/DsCatalog/services/CategoryService.java
@@ -5,13 +5,17 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ProjectDsCatalog.DsCatalog.dto.CategoryDTO;
 import com.ProjectDsCatalog.DsCatalog.entities.Category;
 import com.ProjectDsCatalog.DsCatalog.repositories.CategoryRepository;
-import com.ProjectDsCatalog.DsCatalog.services.exceptions.EntityNotFoundException;
+import com.ProjectDsCatalog.DsCatalog.services.exceptions.DatabaseException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 
 
@@ -41,6 +45,33 @@ public class CategoryService {
 		entity.setName(dto.getName());
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
+	}
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+		Category entity = repository.getReferenceById(id);
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new EntityNotFoundException("Id not found" + id);
+			
+		}
+		
+	}
+
+	
+	public void delete(Long id) {
+		try {
+		repository.deleteById(id);
+	}catch(EmptyResultDataAccessException e) {
+		throw new EntityNotFoundException("Id not found" + id);
+	}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+			
+		}
 	}
 
 }
