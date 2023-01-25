33f4c51bb7609d71406b34811ec86de6acf5ce5f
LeoDominici71 <leonardo_requena@outlook.com>
2023-01-25 04:35:18 +0000

    get by id

--- src/main/java/com/ProjectDsCatalog/DsCatalog/services/CategoryService.java
+++ src/main/java/com/ProjectDsCatalog/DsCatalog/services/CategoryService.java
@@ -2,6 +2,7 @@ package com.ProjectDsCatalog.DsCatalog.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,13 @@ public class CategoryService {
 	 return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 
 		
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
 	}
 
 }
